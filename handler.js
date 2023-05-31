647d2a09f48a100f725ee9d2afb78ee49e5e1d2b
AdithyaB <1999adithya@gmail.com>
2023-05-31 10:21:47 +0530

    v1

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.logger = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Serverless CI/CD Demo',
-      version: "v8.0",
+      version: "v1.0",
       timestamp: moment().unix()
     })
   };
